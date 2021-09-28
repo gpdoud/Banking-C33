937ac1b5435126fb813662834030315c72e9642d
Greg Doud <gpdoud@gmail.com>
2021-09-28 15:58:28 -0400

    Add project files.

---
+++ Banking-C33/Account.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking_C33
+{
+	class Account
+	{
+		public int Id { get; set; }
+		public decimal Balance { get; set; } = 0;
+		public string Description { get; set; }
+
+		public bool Deposit(decimal amount)
+		{
+			this.Balance = this.Balance + amount;
+			return true;
+		}
+		public bool Withdraw(decimal amount)
+		{
+			if(amount > this.Balance)
+			{
+				Console.WriteLine("Insufficient funds!");
+				return false;
+			}
+			this.Balance = this.Balance - amount;
+			return true;
+		}
+		public bool Transfer(decimal amount, Account ToAccount)
+		{
+			var success = Withdraw(amount);
+			if(success == true)
+			{
+				ToAccount.Deposit(amount);
+			}
+			return true;
+		}
+		public void Print()
+		{
+			Console.WriteLine($"{Id} | {Balance} | {Description}");
+		}
+	}
+}
---
+++ Banking-C33/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Banking_C33
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var acct1 = new Account() { 
+				Id = 1, Description = "Test Account"
+			};
+			acct1.Print();
+			acct1.Deposit(9500);
+			acct1.Print();
+			acct1.Withdraw(6000);
+			acct1.Print();
+			acct1.Withdraw(4000);
+			acct1.Print();
+			var acct2 = new Account()
+			{
+				Id = 2,
+				Description = "New Account"
+			};
+			acct1.Transfer(1500, acct2);
+			acct1.Print();
+			acct2.Print();
+
+			acct1.Withdraw(-1000);
+			acct1.Print();
+
+		}
+	}
+}
